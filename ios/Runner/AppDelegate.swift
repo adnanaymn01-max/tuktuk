e1894a0828d3c704d0f765f0e6e5065339320539
adnanaymn01-max <adnanaymn01@gmail.com>
2026-08-22 04:56:33 +0300

    Initialize Google Maps API in AppDelegate
    Added Google Maps API key initialization in AppDelegate.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
@@ -7,6 +8,9 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    GMSServices.provideAPIKey("AIzaSyBA2q57y-msk3udlhSkT1elJ-2OESCecwU")
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
